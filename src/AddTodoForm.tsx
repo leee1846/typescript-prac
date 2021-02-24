2ad932e744e9d00ff124d30998877b1cd35d49f5
leee1846 <leee1846@gmail.com>
2021-02-24 19:43:39 +0900

    feature: addTodo 버튼 클릭 후 input 지우기

--- src/AddTodoForm.tsx
+++ src/AddTodoForm.tsx
@@ -14,6 +14,7 @@ const AddTodoForm: React.FC<AddTodoFormProps> = ({ addTodo }) => {
   const handleSubmit = (e: FormEvent<HTMLButtonElement>) => {
     e.preventDefault();
     addTodo(newTodo);
+    setNewTodo("");
   };
 
   return (
